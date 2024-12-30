c1e7380651cda31b2df68eedb1c727ae6b846658
jd0617 <jaden6171998@gmail.com>
2024-12-30 14:53:32 +0900

    Update

---
+++ docker/docker_rootless_setup.sh
@@ -0,0 +1,17 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y dbus-user-session
+
+sudo apt install -y uidmap
+
+sudo systemctl disable --now docker.service docker.socket
+sudo rm /var/run/docker.sock
+
+dockerd-rootless-setuptool.sh install
+
+export PATH=/usr/bin:$PATH
+export DOCKER_HOST=unix:///run/user/$(id -u)/docker.sock
+
+source ~/.bashrc
+
+docker info | grep Rootless
