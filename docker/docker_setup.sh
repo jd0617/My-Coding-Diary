dbd9ef72f808a7551b356cdb747b4ce9012dc96e
jd0617 <jaden6171998@gmail.com>
2024-12-30 14:24:55 +0900

    Update

--- docker/docker_setup.sh
+++ docker/docker_setup.sh
@@ -13,7 +13,7 @@ echo \
 sudo apt-get update
 
 # Install Docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Verify that the installation is successful
 sudo docker run hello-world
