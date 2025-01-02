ef8222bda4f4b1d5839259bc59cf1ca83c0dc04b
ZhunGee Ong <50040564+jd0617@users.noreply.github.com>
2025-01-02 16:08:43 +0900

    Create docker_uninstall.sh

---
+++ docker/docker_uninstall.sh
@@ -0,0 +1,7 @@
+sudo apt-get purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+
+sudo rm /etc/apt/sources.list.d/docker.list
+sudo rm /etc/apt/keyrings/docker.asc
